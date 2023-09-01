5087560fb21d5abaa433830c8e0621536642e8b4
Levenie <ailalevenieacosta123@gmail.com>
2023-09-02 07:02:07 +0800

    Login page UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
 import { Header } from "./components/Header";
+import { Login } from "./components/Login";
 
 function App() {
   return (
     <div className="App">
-      <Header></Header>
+      {/* <Header></Header> */}
+      <Login />
     </div>
   );
 }
--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,3 +1,95 @@
-import React from "react";
+import * as React from "react";
+import "./Login.css";
+import { Copyright } from "@mui/icons-material";
+import {
+  Grid,
+  Paper,
+  Box,
+  Avatar,
+  Typography,
+  TextField,
+  FormControlLabel,
+  Checkbox,
+  Button,
+  Link,
+  CssBaseline,
+} from "@mui/material";
 
-export const Login = () => {};
+export const Login = () => {
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    console.log({
+      email: data.get("email"),
+      password: data.get("password"),
+    });
+  };
+  return (
+    <div id="main-container">
+      <div id="left">
+        <img className="machines" src={require("./img-loginpage.png")} />
+      </div>
+      <div id="right">
+        <Grid
+          id="signin"
+          item
+          xs={12}
+          sm={8}
+          md={5}
+          component={Paper}
+          elevation={6}
+          square
+        >
+          <Box
+            sx={{
+              my: 8,
+              mx: 4,
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+            }}
+          >
+            <Typography component="h1" variant="h5">
+              Sign in
+            </Typography>
+            <Box
+              component="form"
+              noValidate
+              onSubmit={handleSubmit}
+              sx={{ mt: 1 }}
+            >
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                id="email"
+                label="Email Address"
+                name="email"
+                autoComplete="email"
+                autoFocus
+              />
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+              />
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={{ mt: 3, mb: 2 }}
+              >
+                Sign In
+              </Button>
+            </Box>
+          </Box>
+        </Grid>
+      </div>
+    </div>
+  );
+};
